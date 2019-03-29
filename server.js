8206efd42aa856b3a91954f211e4d447e25930e3
go2u-seattle <yopa@Evanui-MacBook-Pro.local>
2019-03-28 23:52:08 -0700

    testing pull request again

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('Hello Go2U!')
 })
 
+app.get('/test/', (req, res) => {
+    res.send('Hello Test Go2U :)')
+})
+
 app.listen(3000, () => {
     console.log('Listening on Port 3000')
 })
