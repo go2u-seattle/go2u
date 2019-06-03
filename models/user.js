1bc5b1fe4c914b7e835280fa6df3ae81f8816ecc
epseattle <evan.y.park@gmail.com>
2019-06-02 23:24:32 -0700

    adding pushtoken to user model

--- models/user.js
+++ models/user.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: uuidv1()
   },
+  pushToken: {
+    type: String,
+    default: null
+  },
   name: {
     lastName: String,
     middleName: String,
